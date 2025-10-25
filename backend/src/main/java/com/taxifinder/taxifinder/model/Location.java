8b0883a4758845cffc9e888b9b4d9e9f2f49d05e
Vhuhwavho <denganetshi@gmail.com>
2025-10-25 15:58:35 +0200

    changes

---
+++ backend/src/main/java/com/taxifinder/taxifinder/model/Location.java
@@ -0,0 +1,34 @@
+package com.taxifinder.taxifinder.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "locations")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private double latitude;
+
+    @Column(nullable = false)
+    private double longitude;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+}
