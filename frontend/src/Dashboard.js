2b979087a03a6d38832d711223e1a14c66089ff9
Vhuhwavho <denganetshi@gmail.com>
2025-10-25 15:56:05 +0200

    added a dashborad

---
+++ frontend/src/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>TaxiFinder Dashboard</h1>
+      <p>Welcome to your dashboard. Taxi locations will be displayed here.</p>
+      {/* We will add a map and other components here later */}
+    </div>
+  );
+}
+
+export default Dashboard;
