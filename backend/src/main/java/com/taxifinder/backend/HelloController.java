df7015658846538731bea4b71e5f9fbeaea0ad30
Vhuhwavho <denganetshi@gmail.com>
2025-10-29 14:09:00 +0200

    receive from frontend

--- backend/src/main/java/com/taxifinder/backend/HelloController.java
+++ backend/src/main/java/com/taxifinder/backend/HelloController.java
@@ -1,7 +1,11 @@
 package com.taxifinder.backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -9,5 +13,13 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
+    }
+
+    @PostMapping("/api/locations")
+    public void receiveLocation(@RequestBody Map<String, String> payload) {
+        String from = payload.get("from");
+        String to = payload.get("to");
+
+        System.out.println("Received a request to find a taxi from: " + from + " to: " + to);
     }
 }
